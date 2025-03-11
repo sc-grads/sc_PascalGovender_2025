3f68ad27cf15eaf5b2d9b9cc79721a9ba885d679
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-03-11 15:54:38 +0200

    Different joins script added

---
+++ DatabaseAdministration/SQLStatements/Different_JOINS.sql
@@ -0,0 +1,60 @@
+
+--Table creation and select statements--
+CREATE TABLE [dbo].[Course](
+	[CourseID] [int] NULL,
+	[RollNO] [int] NULL
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[Student](
+	[RollNo] [int] NOT NULL,
+	[StudentName] [nvarchar](50) NULL,
+	[StudentCity] [nvarchar](20) NULL,
+	[StudentPhoneNo] [nvarchar](50) NULL,
+	[StudentAge] [int] NULL,
+ CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED 
+(
+	[RollNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+--------------------------------------------------------
+select * from [dbo].[Student]
+select * from [dbo].[Course]
+
+--INNER JOIN--
+select * from [dbo].[Student] s
+inner join [dbo].[Course] c 
+on s.RollNo = c.RollNO
+
+
+select s.RollNo,s.StudentName,c.CourseID from [dbo].[Student] s
+inner join [dbo].[Course] c 
+on s.RollNo = c.RollNO
+
+--JOIN--
+
+select s.RollNo,s.StudentName,c.CourseID from [dbo].[Student] s
+join [dbo].[Course] c 
+on s.RollNo = c.RollNO
+
+--LEFT JOIN--
+
+select s.RollNo,s.StudentName,c.CourseID from [dbo].[Student] s
+left join [dbo].[Course] c 
+on s.RollNo = c.RollNO
+
+
+--RIGHT JOIN--
+
+select s.RollNo,s.StudentName,c.CourseID from [dbo].[Student] s
+right join [dbo].[Course] c 
+on s.RollNo = c.RollNO
+
+--FULL JOIN--
+
+select s.RollNo,s.StudentName,c.CourseID from [dbo].[Student] s
+full join [dbo].[Course] c 
+on s.RollNo = c.RollNO
