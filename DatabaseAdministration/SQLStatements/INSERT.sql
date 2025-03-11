5824dad4bea48e56d1a048bbc1546ef0e35fd46d
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-03-11 17:03:23 +0200

    Corrected naming conventions of files

--- DatabaseAdministration/SQLStatements/INSERT.sql
+++ DatabaseAdministration/SQLStatements/INSERT.sql
@@ -34,27 +34,3 @@ SELECT * FROM salesStaffNew
 INSERT INTO [dbo].[salesStaffNew] (STAFFID,FNAME,LNAME) VALUES (200,'Abbas','Mehmood')
 
 INSERT INTO [dbo].[salesStaffNew] (STAFFID,FNAME,LNAME) VALUES (300,'Imran','Afzal'),(325,'John','Vick'),(314,'James','Dino')
-
------------------------------------
-
---nameOnlyTable insert--
-
-CREATE TABLE [dbo].[nameOnlyTable](
-	
-	[fName] [nvarchar](30),
-	[lName] [nvarchar](30)
-	)
-
-
-GO
-
-SELECT * FROM [nameOnlyTable]
-
--------------------------------------
-
- INSERT INTO nameOnlyTable (fname,lname)
-SELECT fname,lname FROM salesStaffNew WHERE id >= 3
-
---------------------------------
-
-SELECT * into salesStaffNew_bkp FROM salesStaffNew
