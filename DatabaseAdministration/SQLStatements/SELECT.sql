cb6e4ec226953464b7c928e827963f3195fa39b4
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-03-11 14:15:45 +0200

    added select statements

---
+++ DatabaseAdministration/SQLStatements/SELECT.sql
@@ -0,0 +1,9 @@
+--simple select queries--
+
+SELECT * FROM Person.Address;
+
+SELECT AddressID,city,modifieddate FROM person.Address;
+
+SELECT city,addressid,modifieddate FROM person.Address;
+
+SELECT TOP 10 * FROM Person.Address;
