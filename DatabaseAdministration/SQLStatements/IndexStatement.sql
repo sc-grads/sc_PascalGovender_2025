34543db2e02dec27b8b98b3cfbcbe3df57c2f6e7
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-03-12 14:58:30 +0200

    Scripts on indexing via editor added including fragmentation listings

---
+++ DatabaseAdministration/SQLStatements/IndexStatement.sql
@@ -0,0 +1,23 @@
+
+--Go to indexes at table in a DB-
+--Right click folder, new index and use editor--
+--then click scipt once done--
+
+USE [AdventureWorks2022]
+
+GO
+
+SET ANSI_PADDING ON
+
+
+GO
+
+CREATE NONCLUSTERED INDEX [idx_FirstName_LastName] ON [Person].[Person]
+(
+	[FirstName] ASC
+)
+INCLUDE([LastName],[ModifiedDate]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+
+GO
+
+
