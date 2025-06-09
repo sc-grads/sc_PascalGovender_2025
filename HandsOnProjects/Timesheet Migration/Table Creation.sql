3187096492f53415dd8e93efb9125a1aa1784ec6
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-06-09 13:12:51 +0200

    deleted old project files and added new files including new package with update sql script

--- HandsOnProjects/Timesheet Migration/Table Creation.sql
+++ HandsOnProjects/Timesheet Migration/Table Creation.sql
@@ -1,28 +1,30 @@
 USE [TimesheetDB]
 Go
 
---Reset tables
---TRUNCATE TABLE [dbo].[Timesheet];
---TRUNCATE TABLE Leave;
---TRUNCATE TABLE AuditLog;
---TRUNCATE TABLE ErrorLog;
 
--- View data in all tables
---Select * from [dbo].[Timesheet]
---Select * from [dbo].[Leave]
---Select * from [dbo].[AuditLog]
---Select * from [dbo].[ErrorLog]
+DROP TABLE IF EXISTS [dbo].[Employee]
+Go
+CREATE TABLE Employee (
+	 EmployeeID INT PRIMARY KEY IDENTITY(1,1),
+	 EmployeeName NVARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS [dbo].[Client]
+Go
+CREATE TABLE Client (
+	 ClientID INT PRIMARY KEY IDENTITY(1,1),
+	 ClientName NVARCHAR(100) NOT NULL
+);
 
 DROP TABLE IF EXISTS [dbo].[Timesheet]
 Go
--- Timesheets Table
 
 CREATE TABLE Timesheet (
     TimesheetID INT PRIMARY KEY IDENTITY(1,1),
-    EmployeeName NVARCHAR(50) NOT NULL,
+    EmployeeID INT NOT NULL,
     Date DATE NOT NULL,
     DayOfWeek NVARCHAR(50) NOT NULL,
-    Client NVARCHAR(50) NULL,
+    ClientID INT NULL,
     ClientProjectName NVARCHAR(50),
     Description NVARCHAR(50) NULL,
     Billable NVARCHAR(50) NULL,
@@ -30,7 +32,9 @@ CREATE TABLE Timesheet (
     TotalHours DECIMAL(5,2) NOT NULL,
     StartTime TIME(0),
     EndTime TIME(0),
-    CONSTRAINT UQTimesheetUniqueEntry UNIQUE (EmployeeName, Date, StartTime, EndTime)
+    CONSTRAINT UQTimesheetUniqueEntry UNIQUE (EmployeeID, Date, StartTime, EndTime),
+	CONSTRAINT FKTimesheetEmployee FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID),
+    CONSTRAINT FKClient FOREIGN KEY (ClientID) REFERENCES Client(ClientID)
 );
 
 DROP TABLE IF EXISTS [dbo].[Leave]
@@ -38,14 +42,15 @@ Go
 
 CREATE TABLE Leave (
     LeaveID INT PRIMARY KEY IDENTITY(1,1),
-	EmployeeName NVARCHAR(50) NOT NULL,
+	EmployeeID INT NOT NULL,
     Type NVARCHAR(50) NOT NULL,
     StartDate DATE NOT NULL,
     EndDate Date NOT NULL,
     NumberDays INT NOT NULL,
     Approved NVARCHAR(50) NOT NULL,
 	SickNote NVARCHAR(50) NULL
-	CONSTRAINT UQLeaveUniqueEntry UNIQUE (EmployeeName, StartDate, EndDate)
+	CONSTRAINT UQLeaveUniqueEntry UNIQUE (EmployeeID, StartDate, EndDate),
+	CONSTRAINT FKLeaveEmployee FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
 )
 
 DROP TABLE IF EXISTS [dbo].[AuditLog]
@@ -55,7 +60,7 @@ CREATE TABLE AuditLog (
     LogID INT PRIMARY KEY IDENTITY(1,1),
     TableName NVARCHAR(50)NULL,
     Timestamp DATETIME NULL DEFAULT GETDATE(),
-    EmployeeName NVARCHAR(50),
+    EmployeeID INT,
     UserName NVARCHAR(50) NULL,
     Details NVARCHAR(MAX)
 );
@@ -70,3 +75,19 @@ CREATE TABLE ErrorLog (
 	ErrorDescription NVARCHAR(1000),
 	Timestamp DATETIME DEFAULT GETDATE()
 );
+
+--Reset tables
+TRUNCATE TABLE [dbo].[Timesheet];
+TRUNCATE TABLE Leave;
+TRUNCATE TABLE AuditLog;
+TRUNCATE TABLE ErrorLog;
+TRUNCATE TABLE Client;
+TRUNCATE TABLE Employee;
+
+-- View data in all tables
+Select * from [dbo].[Timesheet]
+Select * from [dbo].[Leave]
+Select * from [dbo].[AuditLog]
+Select * from [dbo].[ErrorLog]
+Select * from [dbo].Client
+Select * from [dbo].Employee
