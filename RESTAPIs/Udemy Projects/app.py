9d16c195b7926a7ef01b07591bfe5d0e9a66c047
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-06-30 17:32:57 +0200

    Update .gitignore, modify Dockerfile and docker-compose.yml, and enhance app.py for environment variable support; add description column to ItemModel and create migration scripts

--- RESTAPIs/Udemy Projects/app.py
+++ RESTAPIs/Udemy Projects/app.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import JWTManager
 from blocklist import BLOCKLIST
 from db import db
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 import models
 
@@ -17,15 +18,16 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()  # Load environment variables from .env file
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config[ "OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
