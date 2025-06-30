ed8e8bd45261395e3c6b5227b2c0824cc0ebf7b6
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-06-30 08:59:07 +0200

    Add JWT authentication and user login functionality
    - Implemented JWT token handling with expiration and invalidation callbacks.
    - Secured item routes with JWT authentication.
    - Added user login endpoint to generate access tokens.

--- RESTAPIs/Udemy Projects/app.py
+++ RESTAPIs/Udemy Projects/app.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -58,8 +58,38 @@ def create_app(db_url=None):
     db.init_app(app)
     api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(128))
+
+    app.config["JWT_SECRET_KEY"] = "jose"
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
 
 
     with app.app_context():
--- RESTAPIs/Udemy Projects/resources/user.py
+++ RESTAPIs/Udemy Projects/resources/user.py
@@ -1,5 +1,6 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extended import create_access_token
 from passlib.hash import pbkdf2_sha256
 
 from db import db
@@ -9,6 +10,19 @@ from schemas import UserSchema
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=str(user.id))
+            return {"access_token": access_token}, 200
+
+        abort(401, message="Invalid credentials.")
 
 @blp.route("/register")
 class UserRegister(MethodView):
