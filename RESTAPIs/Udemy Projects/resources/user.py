0eea7af44060d2def303c54abd0621db33d92438
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-07-01 10:17:45 +0200

    added old files back

---
+++ RESTAPIs/Udemy Projects/resources/user.py
@@ -0,0 +1,114 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt_identity,
+    get_jwt,
+    jwt_required,
+)
+
+from db import db
+from models import UserModel
+from schemas import UserSchema, UserRegisterSchema
+from blocklist import BLOCKLIST
+from sqlalchemy import or_ 
+
+import os
+import requests
+
+
+blp = Blueprint("Users", "users", description="Operations on users")
+
+def send_simple_message(to,subject,body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+  		"https://api.mailgun.net/v3/{domain}/messages",
+  		auth=("api", os.getenv("MAILGUN_API_KEY")),
+  		data={"from": "Pascal Govender <postmaster@{domain}>",
+			"to": [to],
+  			"subject": subject,
+  			"text": body})
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=str(user.id), fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
+        abort(401, message="Invalid credentials.")
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        # Make it clear that when to add the refresh token to the blocklist will depend on the app design
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"access_token": new_token}, 200
+    
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserRegisterSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(
+            or_(
+            UserModel.email == user_data["email"],
+            UserModel.username == user_data["username"]
+            )).first():
+            abort(409, message="A user with that username already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            email=user_data["email"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="successful registered",
+            body=f"Hello {user_data['username']},\n\nThank you for registering with us!"
+        )
+
+
+        return {"message": "User created successfully."}, 201
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    """
+    This resource can be useful when testing our Flask app.
+    We may not want to expose it to public users, but for the
+    sake of demonstration in this course, it can be useful
+    when we are manipulating data regarding the users.
+    """
+
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}, 200
