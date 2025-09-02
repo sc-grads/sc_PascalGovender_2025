05d1815a1137fc623467de664abd3f0d42f15735
Pascal Govender <pascal.govender@sambeconsulting.com>
2025-09-02 08:42:37 +0200

    updated C# folder

---
+++ C#/StudentRegistrationProject/StudentRegistrationProject/StudentData.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentRegistrationProject
+{
+    /// <summary>
+    /// Interaction logic for StudentData.xaml
+    /// </summary>
+    /// 
+    public partial class StudentData : Window
+    {
+        public StudentData()
+        {
+            InitializeComponent();
+            ConnectToDBs ctDB = new ConnectToDBs();
+            dataGridStudents.ItemsSource = ctDB.Displaystudent().DefaultView;
+        }
+
+        private void btnRefreshData_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToDBs ctDB = new ConnectToDBs();
+            dataGridStudents.ItemsSource = ctDB.Displaystudent().DefaultView;
+        }
+    }
+}
